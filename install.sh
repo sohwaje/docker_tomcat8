6357a10b0cc1e7d7b434eb66961583a32473dd1c
sigongweb <kisssulran@i-screammedia.com>
2020-10-05 11:09:22 +0900

    Fport,Bport

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ date_=$(date "+%Y%m%d%H%M%S")
 IMAGE="tomcat8"
 CONTAINER="tomcat8"
 LOGDIR="/var/log/$CONTAINER"
-
-
+Fport="18080"
+Bport="8080"
 _retVal()
 {
   local retVal=$?
@@ -22,22 +22,23 @@ _retVal()
 ### start
 echo "[1] Docker install check"
 echo "[Step 1 ---> Docker install check]"
-  sudo docker -v >/dev/null 2>&1 || _retVal
+  sudo docker -v >/dev/null 2>&1
+  _retVal
 
 echo "[2] Create Tomcat log directory"
-if [[ -d $LOGDIR ]];then
+if [[ ! -d $LOGDIR ]];then
+  echo "[Step 2 ---> Create $LOGDIR]"
+  sudo mkdir /var/log/$LOGDIR
+else
   echo "[Step 2 ---> $LOGDIR log directory already exist. backup and create]"
   sudo mv $LOGDIR /var/log/$CONTAINER-$date_
   sudo mkdir $LOGDIR
-else
-  echo "[Step 2 ---> Create $LOGDIR]"
-  sudo mkdir /var/log/$LOGDIR
 fi
 
 echo "[3] install tomcat8 docker"
 echo "[Step 3 ---> docker build & run]"
 docker build -t $IMAGE $BASEDIR/ && \
-  docker run -d -p 18080:8080 \
+  docker run -d -p $Fport:$Bport \
   --name $CONTAINER \
   -v $LOGDIR:/usr/local/tomcat/logs \
   $IMAGE
